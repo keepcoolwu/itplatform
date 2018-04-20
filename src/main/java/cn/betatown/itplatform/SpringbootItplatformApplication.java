f5b2195df2460046249f8bd587d451c2906e0586
keepcoolwu <wu_zheng_jun@163.com>
2018-04-20 16:51:16 +0800

    加入页面

--- src/main/java/cn/betatown/itplatform/SpringbootItplatformApplication.java
+++ src/main/java/cn/betatown/itplatform/SpringbootItplatformApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 @MapperScan(basePackages = { "cn.betatown.itplatform.mapper", "cn.betatown.itplatform.security.mapper" })
 public class SpringbootItplatformApplication extends SpringBootServletInitializer {
--- src/main/java/cn/betatown/itplatform/security/config/DataSourceConfig.java
+++ src/main/java/cn/betatown/itplatform/security/config/DataSourceConfig.java
@@ -19,20 +19,20 @@ import javax.sql.DataSource;
  * @项目名称：wyait-common
  * @包名：com.wyait.manage.config
  * @类描述：数据源配置
- * @创建人：wyait
- * @创建时间：2018-02-27 13:33
- * @version：V1.0
+ * @创建人：wyait @创建时间：2018-02-27 13:33 @version：V1.0
  */
 @Configuration
-//指明了扫描dao层，并且给dao层注入指定的SqlSessionTemplate
-@MapperScan(basePackages = "com.wyait.manage.dao", sqlSessionTemplateRef  = "testSqlSessionTemplate")
+// 指明了扫描dao层，并且给dao层注入指定的SqlSessionTemplate
+@MapperScan(basePackages = "cn.betatown.itplatform.security.dao", sqlSessionTemplateRef = "testSqlSessionTemplate")
 public class DataSourceConfig {
+
 	/**
 	 * 创建datasource对象
+	 * 
 	 * @return
 	 */
 	@Bean(name = "testDataSource")
-	@ConfigurationProperties(prefix = "slave.datasource.test")// prefix值必须是application.properteis中对应属性的前缀
+	@ConfigurationProperties(prefix = "slave.datasource.test") // prefix值必须是application.properteis中对应属性的前缀
 	@Primary
 	public DataSource testDataSource() {
 		return DataSourceBuilder.create().build();
@@ -40,26 +40,29 @@ public class DataSourceConfig {
 
 	/**
 	 * 创建sql工程
+	 * 
 	 * @param dataSource
 	 * @return
 	 * @throws Exception
 	 */
 	@Bean(name = "testSqlSessionFactory")
 	@Primary
-	public SqlSessionFactory testSqlSessionFactory(@Qualifier("testDataSource") DataSource dataSource) throws Exception {
+	public SqlSessionFactory testSqlSessionFactory(@Qualifier("testDataSource") DataSource dataSource)
+			throws Exception {
 		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
 		bean.setDataSource(dataSource);
-		//对应mybatis.type-aliases-package配置
+		// 对应mybatis.type-aliases-package配置
 		bean.setTypeAliasesPackage("com.wyait.manage.pojo");
-		//对应mybatis.mapper-locations配置
+		// 对应mybatis.mapper-locations配置
 		bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
-		//开启驼峰映射
+		// 开启驼峰映射
 		bean.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
 		return bean.getObject();
 	}
 
 	/**
 	 * 配置事务管理
+	 * 
 	 * @param dataSource
 	 * @return
 	 */
@@ -71,13 +74,15 @@ public class DataSourceConfig {
 
 	/**
 	 * sqlSession模版，用于配置自动扫描pojo实体类
+	 * 
 	 * @param sqlSessionFactory
 	 * @return
 	 * @throws Exception
 	 */
 	@Bean(name = "testSqlSessionTemplate")
 	@Primary
-	public SqlSessionTemplate testSqlSessionTemplate(@Qualifier("testSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+	public SqlSessionTemplate testSqlSessionTemplate(
+			@Qualifier("testSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
 		return new SqlSessionTemplate(sqlSessionFactory);
 	}
 }
