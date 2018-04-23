5af239d5870703988a16a8c9394e6c53d016c162
keepcoolwu <wu_zheng_jun@163.com>
2018-04-23 17:39:42 +0800

    解决菜单问题

--- src/main/java/cn/betatown/itplatform/security/web/IndexController.java
+++ src/main/java/cn/betatown/itplatform/security/web/IndexController.java
@@ -8,35 +8,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @项目名称：lyd-channel
- * @包名：com.lyd.channel.web
- * @类描述：
- * @创建人：wyait
- * @创建时间：2017-11-28 18:52
- * @version：V1.0
+ * @包名：com.lyd.channel.web @类描述：
+ * @创建人：wyait @创建时间：2017-11-28 18:52 @version：V1.0
  */
 @Controller
 @RequestMapping("/")
 public class IndexController {
-	private static final Logger logger = LoggerFactory
-			.getLogger(IndexController.class);
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-	@RequestMapping("/index") public String index() {
+	@RequestMapping("/index")
+	public String index() {
 		logger.debug("-------------index------------");
 		return "index";
 	}
 
-	@RequestMapping("/home") public String toHome() {
+	@RequestMapping("/home")
+	public String toHome() {
 		logger.debug("===111-------------home------------");
 		return "home";
 	}
+
 	@RequestMapping("/login")
 	public String toLogin() {
 		logger.debug("===111-------------login------------");
 		return "login";
 	}
 
-	@RequestMapping("/{page}") public String toPage(
-			@PathVariable("page") String page) {
+	@RequestMapping("/{page}")
+	public String toPage(@PathVariable("page") String page) {
 		logger.debug("-------------toindex------------" + page);
 		return page;
 	}
