6d1d5c454c28b712780e573125abbebe66049ff6
keepcoolwu <wu_zheng_jun@163.com>
2018-04-23 17:01:32 +0800

    修改log配置文件

--- src/main/java/cn/betatown/itplatform/SpringbootItplatformApplication.java
+++ src/main/java/cn/betatown/itplatform/SpringbootItplatformApplication.java
@@ -1,6 +1,5 @@
 package cn.betatown.itplatform;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
